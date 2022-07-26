e2d69a021ff41ea61df4033e986ce0bd240996c2
FrontendCreater <frontend-dev@list.ru>
2022-07-27 00:47:01 +0500

    add fratures

---
+++ js/index.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper('.main-slider', {
+    slidesPerView: 1,
+    loop: true,
+  
+    autoplay: {
+        delay: 5000,
+    },
+
+    breakpoints: {
+      768: {
+        direction: 'vertical',
+      }
+    },
+  
+    pagination: {
+      el: '.main-slider__pagination',
+      clickable: true,
+    },
+});
