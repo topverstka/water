ee000680730c4b5982b63f0bdee26ba2814ab432
FrontendCreater <frontend-dev@list.ru>
2022-07-27 13:09:40 +0500

    add 2 slider

--- js/index.js
+++ js/index.js
@@ -2,15 +2,15 @@ const swiper = new Swiper('.main-slider', {
     slidesPerView: 1,
     loop: true,
   
-    autoplay: {
-        delay: 5000,
-    },
+    // autoplay: {
+    //     delay: 5000,
+    // },
 
-    breakpoints: {
-      768: {
-        direction: 'vertical',
-      }
-    },
+    // breakpoints: {
+    //   768: {
+    //     direction: 'vertical',
+    //   }
+    // },
   
     pagination: {
       el: '.main-slider__pagination',
